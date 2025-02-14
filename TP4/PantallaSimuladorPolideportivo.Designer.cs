206f564d0b0ca1f12a73c3edd394cd10161f77a7
Marina-Brasca <81325595+84421-Marina-Brasca@users.noreply.github.com>
2025-02-14 00:54:50 -0300

    feat: interface

---
+++ TP4/PantallaSimuladorPolideportivo.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace TP4
+{
+    partial class PantallaSimParametros
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_tiempoSimulacion = new Label();
+            txt_tiempoSimulacion = new TextBox();
+            lbl_unidadTSim = new Label();
+            txt_horaDesde = new TextBox();
+            lbl_horaDesde = new Label();
+            lbl_tipoPaciente = new Label();
+            lbl_Consulta = new Label();
+            lbl_Urgencia = new Label();
+            lbl_llegadas = new Label();
+            lbl_finAtencion = new Label();
+            btn_generar = new Button();
+            txt_B_finAtencUrgencia = new TextBox();
+            lbl_distoOcupHB3 = new Label();
+            lbl_distoOcupHB2 = new Label();
+            txt_A_finAtencUrgencia = new TextBox();
+            lbl_distoOcupHB1 = new Label();
+            txt_B_finAtencConsulta = new TextBox();
+            lbl_distoOcupF3 = new Label();
+            lbl_distoOcupF2 = new Label();
+            txt_A_finAtencConsulta = new TextBox();
+            lbl_distoOcupF1 = new Label();
+            txt_expLlegConsulta = new TextBox();
+            lbl_distLlegF1 = new Label();
+            lbl_distLlegF2 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txt_expLlegUrgencia = new TextBox();
+            SuspendLayout();
+            // 
+            // lbl_tiempoSimulacion
+            // 
+            lbl_tiempoSimulacion.AutoSize = true;
+            lbl_tiempoSimulacion.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_tiempoSimulacion.Location = new Point(64, 50);
+            lbl_tiempoSimulacion.Name = "lbl_tiempoSimulacion";
+            lbl_tiempoSimulacion.Size = new Size(127, 15);
+            lbl_tiempoSimulacion.TabIndex = 0;
+            lbl_tiempoSimulacion.Text = "Tiempo de simulación";
+            // 
+            // txt_tiempoSimulacion
+            // 
+            txt_tiempoSimulacion.Location = new Point(66, 71);
+            txt_tiempoSimulacion.Name = "txt_tiempoSimulacion";
+            txt_tiempoSimulacion.Size = new Size(142, 23);
+            txt_tiempoSimulacion.TabIndex = 1;
+            txt_tiempoSimulacion.Text = "50";
+            txt_tiempoSimulacion.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_unidadTSim
+            // 
+            lbl_unidadTSim.AutoSize = true;
+            lbl_unidadTSim.Location = new Point(228, 75);
+            lbl_unidadTSim.Name = "lbl_unidadTSim";
+            lbl_unidadTSim.Size = new Size(36, 15);
+            lbl_unidadTSim.TabIndex = 2;
+            lbl_unidadTSim.Text = "horas";
+            // 
+            // txt_horaDesde
+            // 
+            txt_horaDesde.Location = new Point(66, 122);
+            txt_horaDesde.Name = "txt_horaDesde";
+            txt_horaDesde.Size = new Size(142, 23);
+            txt_horaDesde.TabIndex = 6;
+            txt_horaDesde.Text = "0";
+            txt_horaDesde.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_horaDesde
+            // 
+            lbl_horaDesde.AutoSize = true;
+            lbl_horaDesde.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_horaDesde.Location = new Point(66, 104);
+            lbl_horaDesde.Name = "lbl_horaDesde";
+            lbl_horaDesde.Size = new Size(139, 15);
+            lbl_horaDesde.TabIndex = 5;
+            lbl_horaDesde.Text = "Mostrar resultados desde";
+            // 
+            // lbl_tipoPaciente
+            // 
+            lbl_tipoPaciente.AutoSize = true;
+            lbl_tipoPaciente.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_tipoPaciente.Location = new Point(68, 194);
+            lbl_tipoPaciente.Name = "lbl_tipoPaciente";
+            lbl_tipoPaciente.Size = new Size(99, 15);
+            lbl_tipoPaciente.TabIndex = 11;
+            lbl_tipoPaciente.Text = "Tipo de Paciente";
+            // 
+            // lbl_Consulta
+            // 
+            lbl_Consulta.AutoSize = true;
+            lbl_Consulta.Location = new Point(68, 230);
+            lbl_Consulta.Name = "lbl_Consulta";
+            lbl_Consulta.Size = new Size(54, 15);
+            lbl_Consulta.TabIndex = 12;
+            lbl_Consulta.Text = "Consulta";
+            // 
+            // lbl_Urgencia
+            // 
+            lbl_Urgencia.AutoSize = true;
+            lbl_Urgencia.Location = new Point(68, 267);
+            lbl_Urgencia.Name = "lbl_Urgencia";
+            lbl_Urgencia.Size = new Size(54, 15);
+            lbl_Urgencia.TabIndex = 13;
+            lbl_Urgencia.Text = "Urgencia";
+            // 
+            // lbl_llegadas
+            // 
+            lbl_llegadas.AutoSize = true;
+            lbl_llegadas.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_llegadas.Location = new Point(202, 194);
+            lbl_llegadas.Name = "lbl_llegadas";
+            lbl_llegadas.Size = new Size(54, 15);
+            lbl_llegadas.TabIndex = 15;
+            lbl_llegadas.Text = "Llegadas";
+            // 
+            // lbl_finAtencion
+            // 
+            lbl_finAtencion.AutoSize = true;
+            lbl_finAtencion.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_finAtencion.Location = new Point(367, 194);
+            lbl_finAtencion.Name = "lbl_finAtencion";
+            lbl_finAtencion.Size = new Size(93, 15);
+            lbl_finAtencion.TabIndex = 16;
+            lbl_finAtencion.Text = "Fin de Atenciòn";
+            // 
+            // btn_generar
+            // 
+            btn_generar.BackColor = Color.DarkBlue;
+            btn_generar.Font = new Font("Arial Black", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_generar.ForeColor = Color.AliceBlue;
+            btn_generar.Location = new Point(299, 51);
+            btn_generar.Name = "btn_generar";
+            btn_generar.Size = new Size(242, 94);
+            btn_generar.TabIndex = 17;
+            btn_generar.Text = "Generar";
+            btn_generar.UseVisualStyleBackColor = false;
+            btn_generar.Click += btn_generar_Click;
+            // 
+            // txt_B_finAtencUrgencia
+            // 
+            txt_B_finAtencUrgencia.Location = new Point(440, 264);
+            txt_B_finAtencUrgencia.Name = "txt_B_finAtencUrgencia";
+            txt_B_finAtencUrgencia.Size = new Size(47, 23);
+            txt_B_finAtencUrgencia.TabIndex = 38;
+            txt_B_finAtencUrgencia.Text = "40";
+            txt_B_finAtencUrgencia.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_distoOcupHB3
+            // 
+            lbl_distoOcupHB3.AutoSize = true;
+            lbl_distoOcupHB3.Location = new Point(493, 267);
+            lbl_distoOcupHB3.Name = "lbl_distoOcupHB3";
+            lbl_distoOcupHB3.Size = new Size(35, 15);
+            lbl_distoOcupHB3.TabIndex = 37;
+            lbl_distoOcupHB3.Text = ") min";
+            // 
+            // lbl_distoOcupHB2
+            // 
+            lbl_distoOcupHB2.AutoSize = true;
+            lbl_distoOcupHB2.Location = new Point(433, 272);
+            lbl_distoOcupHB2.Name = "lbl_distoOcupHB2";
+            lbl_distoOcupHB2.Size = new Size(10, 15);
+            lbl_distoOcupHB2.TabIndex = 36;
+            lbl_distoOcupHB2.Text = ",";
+            // 
+            // txt_A_finAtencUrgencia
+            // 
+            txt_A_finAtencUrgencia.Location = new Point(385, 264);
+            txt_A_finAtencUrgencia.Name = "txt_A_finAtencUrgencia";
+            txt_A_finAtencUrgencia.Size = new Size(47, 23);
+            txt_A_finAtencUrgencia.TabIndex = 35;
+            txt_A_finAtencUrgencia.Text = "25";
+            txt_A_finAtencUrgencia.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_distoOcupHB1
+            // 
+            lbl_distoOcupHB1.AutoSize = true;
+            lbl_distoOcupHB1.Location = new Point(367, 267);
+            lbl_distoOcupHB1.Name = "lbl_distoOcupHB1";
+            lbl_distoOcupHB1.Size = new Size(19, 15);
+            lbl_distoOcupHB1.TabIndex = 34;
+            lbl_distoOcupHB1.Text = "U(";
+            // 
+            // txt_B_finAtencConsulta
+            // 
+            txt_B_finAtencConsulta.Location = new Point(440, 227);
+            txt_B_finAtencConsulta.Name = "txt_B_finAtencConsulta";
+            txt_B_finAtencConsulta.Size = new Size(47, 23);
+            txt_B_finAtencConsulta.TabIndex = 43;
+            txt_B_finAtencConsulta.Text = "20";
+            txt_B_finAtencConsulta.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_distoOcupF3
+            // 
+            lbl_distoOcupF3.AutoSize = true;
+            lbl_distoOcupF3.Location = new Point(493, 230);
+            lbl_distoOcupF3.Name = "lbl_distoOcupF3";
+            lbl_distoOcupF3.Size = new Size(35, 15);
+            lbl_distoOcupF3.TabIndex = 42;
+            lbl_distoOcupF3.Text = ") min";
+            // 
+            // lbl_distoOcupF2
+            // 
+            lbl_distoOcupF2.AutoSize = true;
+            lbl_distoOcupF2.Location = new Point(433, 235);
+            lbl_distoOcupF2.Name = "lbl_distoOcupF2";
+            lbl_distoOcupF2.Size = new Size(10, 15);
+            lbl_distoOcupF2.TabIndex = 41;
+            lbl_distoOcupF2.Text = ",";
+            // 
+            // txt_A_finAtencConsulta
+            // 
+            txt_A_finAtencConsulta.Location = new Point(385, 227);
+            txt_A_finAtencConsulta.Name = "txt_A_finAtencConsulta";
+            txt_A_finAtencConsulta.Size = new Size(47, 23);
+            txt_A_finAtencConsulta.TabIndex = 40;
+            txt_A_finAtencConsulta.Text = "12";
+            txt_A_finAtencConsulta.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_distoOcupF1
+            // 
+            lbl_distoOcupF1.AutoSize = true;
+            lbl_distoOcupF1.Location = new Point(367, 230);
+            lbl_distoOcupF1.Name = "lbl_distoOcupF1";
+            lbl_distoOcupF1.Size = new Size(19, 15);
+            lbl_distoOcupF1.TabIndex = 39;
+            lbl_distoOcupF1.Text = "U(";
+            // 
+            // txt_expLlegConsulta
+            // 
+            txt_expLlegConsulta.Location = new Point(265, 224);
+            txt_expLlegConsulta.Name = "txt_expLlegConsulta";
+            txt_expLlegConsulta.Size = new Size(47, 23);
+            txt_expLlegConsulta.TabIndex = 44;
+            txt_expLlegConsulta.Text = "40";
+            txt_expLlegConsulta.KeyPress += txt_DoubleParam_KeyPress;
+            // 
+            // lbl_distLlegF1
+            // 
+            lbl_distLlegF1.AutoSize = true;
+            lbl_distLlegF1.Location = new Point(202, 227);
+            lbl_distLlegF1.Name = "lbl_distLlegF1";
+            lbl_distLlegF1.Size = new Size(57, 15);
+            lbl_distLlegF1.TabIndex = 45;
+            lbl_distLlegF1.Text = "Exp. Neg.";
+            // 
+            // lbl_distLlegF2
+            // 
+            lbl_distLlegF2.AutoSize = true;
+            lbl_distLlegF2.Location = new Point(318, 230);
+            lbl_distLlegF2.Name = "lbl_distLlegF2";
+            lbl_distLlegF2.Size = new Size(22, 15);
+            lbl_distLlegF2.TabIndex = 46;
+            lbl_distLlegF2.Text = " hs";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(228, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 15);
+            label1.TabIndex = 47;
+            label1.Text = "horas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(318, 269);
+            label2.Name = "label2";
+            label2.Size = new Size(22, 15);
+            label2.TabIndex = 50;
+            label2.Text = " hs";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(202, 266);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 49;
+            label3.Text = "Exp. Neg.";
+            // 
+            // txt_expLlegUrgencia
+            // 
+            txt_expLlegUrgencia.Location = new Point(265, 263);
+            txt_expLlegUrgencia.Name = "txt_expLlegUrgencia";
+            txt_expLlegUrgencia.Size = new Size(47, 23);
+            txt_expLlegUrgencia.TabIndex = 48;
+            txt_expLlegUrgencia.Text = "105";
+            // 
+            // PantallaSimParametros
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(612, 333);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(txt_expLlegUrgencia);
+            Controls.Add(label1);
+            Controls.Add(lbl_distLlegF2);
+            Controls.Add(lbl_distLlegF1);
+            Controls.Add(txt_expLlegConsulta);
+            Controls.Add(txt_B_finAtencConsulta);
+            Controls.Add(lbl_distoOcupF3);
+            Controls.Add(lbl_distoOcupF2);
+            Controls.Add(txt_A_finAtencConsulta);
+            Controls.Add(lbl_distoOcupF1);
+            Controls.Add(txt_B_finAtencUrgencia);
+            Controls.Add(lbl_distoOcupHB3);
+            Controls.Add(lbl_distoOcupHB2);
+            Controls.Add(txt_A_finAtencUrgencia);
+            Controls.Add(lbl_distoOcupHB1);
+            Controls.Add(btn_generar);
+            Controls.Add(lbl_finAtencion);
+            Controls.Add(lbl_llegadas);
+            Controls.Add(lbl_Urgencia);
+            Controls.Add(lbl_Consulta);
+            Controls.Add(lbl_tipoPaciente);
+            Controls.Add(txt_horaDesde);
+            Controls.Add(lbl_horaDesde);
+            Controls.Add(lbl_unidadTSim);
+            Controls.Add(txt_tiempoSimulacion);
+            Controls.Add(lbl_tiempoSimulacion);
+            Name = "PantallaSimParametros";
+            Text = "Ej. Consultorio -  Parametros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_tiempoSimulacion;
+        private TextBox txt_tiempoSimulacion;
+        private Label lbl_unidadTSim;
+        private TextBox txt_horaDesde;
+        private Label lbl_horaDesde;
+        private Label lbl_tipoPaciente;
+        private Label lbl_Consulta;
+        private Label lbl_Urgencia;
+        private Label lbl_llegadas;
+        private Label lbl_finAtencion;
+        private Button btn_generar;
+        private TextBox txt_B_finAtencUrgencia;
+        private Label lbl_distoOcupHB3;
+        private Label lbl_distoOcupHB2;
+        private TextBox txt_A_finAtencUrgencia;
+        private Label lbl_distoOcupHB1;
+        private TextBox txt_B_finAtencConsulta;
+        private Label lbl_distoOcupF3;
+        private Label lbl_distoOcupF2;
+        private TextBox txt_A_finAtencConsulta;
+        private Label lbl_distoOcupF1;
+        private TextBox txt_expLlegConsulta;
+        private Label lbl_distLlegF1;
+        private Label lbl_distLlegF2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txt_expLlegUrgencia;
+    }
+}
---
+++ TP4/PantallaVisualizacion.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TP4
+{
+    partial class PantallaVisualizacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gdw_iteracionesSolicitadas = new DataGridView();
+            lbl_tiempoMaxEspera = new Label();
+            lbl_colasMax = new Label();
+            txt_PorcUrgenciasAtend = new TextBox();
+            txt_colaMaxConsulta = new TextBox();
+            txt_colaMaxUrgencia = new TextBox();
+            txt_colaMaxTotal = new TextBox();
+            lbl_colaMaxTotal = new Label();
+            lbl_colaMaxConsulta = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txt_maxEsperaUrgencia = new TextBox();
+            txt_maxEsperaConsulta = new TextBox();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)gdw_iteracionesSolicitadas).BeginInit();
+            SuspendLayout();
+            // 
+            // gdw_iteracionesSolicitadas
+            // 
+            gdw_iteracionesSolicitadas.AllowUserToAddRows = false;
+            gdw_iteracionesSolicitadas.AllowUserToDeleteRows = false;
+            gdw_iteracionesSolicitadas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gdw_iteracionesSolicitadas.Location = new Point(24, 22);
+            gdw_iteracionesSolicitadas.Name = "gdw_iteracionesSolicitadas";
+            gdw_iteracionesSolicitadas.ReadOnly = true;
+            gdw_iteracionesSolicitadas.RowHeadersWidth = 51;
+            gdw_iteracionesSolicitadas.RowTemplate.Height = 25;
+            gdw_iteracionesSolicitadas.Size = new Size(1137, 415);
+            gdw_iteracionesSolicitadas.TabIndex = 0;
+            // 
+            // lbl_tiempoMaxEspera
+            // 
+            lbl_tiempoMaxEspera.AutoSize = true;
+            lbl_tiempoMaxEspera.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_tiempoMaxEspera.Location = new Point(24, 455);
+            lbl_tiempoMaxEspera.Name = "lbl_tiempoMaxEspera";
+            lbl_tiempoMaxEspera.Size = new Size(268, 17);
+            lbl_tiempoMaxEspera.TabIndex = 1;
+            lbl_tiempoMaxEspera.Text = "Tiempos màximos de espera (en minutos)";
+            // 
+            // lbl_colasMax
+            // 
+            lbl_colasMax.AutoSize = true;
+            lbl_colasMax.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_colasMax.Location = new Point(880, 454);
+            lbl_colasMax.Name = "lbl_colasMax";
+            lbl_colasMax.Size = new Size(100, 17);
+            lbl_colasMax.TabIndex = 2;
+            lbl_colasMax.Text = "Colas Màximas";
+            // 
+            // txt_PorcUrgenciasAtend
+            // 
+            txt_PorcUrgenciasAtend.Enabled = false;
+            txt_PorcUrgenciasAtend.Location = new Point(445, 485);
+            txt_PorcUrgenciasAtend.Name = "txt_PorcUrgenciasAtend";
+            txt_PorcUrgenciasAtend.Size = new Size(297, 23);
+            txt_PorcUrgenciasAtend.TabIndex = 3;
+            // 
+            // txt_colaMaxConsulta
+            // 
+            txt_colaMaxConsulta.Enabled = false;
+            txt_colaMaxConsulta.Location = new Point(940, 485);
+            txt_colaMaxConsulta.Name = "txt_colaMaxConsulta";
+            txt_colaMaxConsulta.Size = new Size(68, 23);
+            txt_colaMaxConsulta.TabIndex = 4;
+            // 
+            // txt_colaMaxUrgencia
+            // 
+            txt_colaMaxUrgencia.Enabled = false;
+            txt_colaMaxUrgencia.Location = new Point(1092, 485);
+            txt_colaMaxUrgencia.Name = "txt_colaMaxUrgencia";
+            txt_colaMaxUrgencia.Size = new Size(69, 23);
+            txt_colaMaxUrgencia.TabIndex = 5;
+            // 
+            // txt_colaMaxTotal
+            // 
+            txt_colaMaxTotal.Enabled = false;
+            txt_colaMaxTotal.Location = new Point(1092, 456);
+            txt_colaMaxTotal.Name = "txt_colaMaxTotal";
+            txt_colaMaxTotal.Size = new Size(69, 23);
+            txt_colaMaxTotal.TabIndex = 6;
+            // 
+            // lbl_colaMaxTotal
+            // 
+            lbl_colaMaxTotal.AutoSize = true;
+            lbl_colaMaxTotal.Location = new Point(1045, 459);
+            lbl_colaMaxTotal.Name = "lbl_colaMaxTotal";
+            lbl_colaMaxTotal.Size = new Size(38, 15);
+            lbl_colaMaxTotal.TabIndex = 8;
+            lbl_colaMaxTotal.Text = "Total :";
+            // 
+            // lbl_colaMaxConsulta
+            // 
+            lbl_colaMaxConsulta.AutoSize = true;
+            lbl_colaMaxConsulta.Location = new Point(880, 489);
+            lbl_colaMaxConsulta.Name = "lbl_colaMaxConsulta";
+            lbl_colaMaxConsulta.Size = new Size(60, 15);
+            lbl_colaMaxConsulta.TabIndex = 9;
+            lbl_colaMaxConsulta.Text = "Consulta :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1027, 489);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 10;
+            label1.Text = "Urgencia :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 489);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Urgencia :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 489);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 15);
+            label3.TabIndex = 13;
+            label3.Text = "Consulta :";
+            // 
+            // txt_maxEsperaUrgencia
+            // 
+            txt_maxEsperaUrgencia.Enabled = false;
+            txt_maxEsperaUrgencia.Location = new Point(221, 484);
+            txt_maxEsperaUrgencia.Name = "txt_maxEsperaUrgencia";
+            txt_maxEsperaUrgencia.Size = new Size(56, 23);
+            txt_maxEsperaUrgencia.TabIndex = 12;
+            // 
+            // txt_maxEsperaConsulta
+            // 
+            txt_maxEsperaConsulta.Enabled = false;
+            txt_maxEsperaConsulta.Location = new Point(86, 485);
+            txt_maxEsperaConsulta.Name = "txt_maxEsperaConsulta";
+            txt_maxEsperaConsulta.Size = new Size(55, 23);
+            txt_maxEsperaConsulta.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(445, 459);
+            label4.Name = "label4";
+            label4.Size = new Size(297, 17);
+            label4.TabIndex = 15;
+            label4.Text = "Porcentaje urgencias atendidas / tot atendidos";
+            // 
+            // PantallaVisualizacion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(1184, 526);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(txt_maxEsperaUrgencia);
+            Controls.Add(txt_maxEsperaConsulta);
+            Controls.Add(label1);
+            Controls.Add(lbl_colaMaxConsulta);
+            Controls.Add(lbl_colaMaxTotal);
+            Controls.Add(txt_colaMaxTotal);
+            Controls.Add(txt_colaMaxUrgencia);
+            Controls.Add(txt_colaMaxConsulta);
+            Controls.Add(txt_PorcUrgenciasAtend);
+            Controls.Add(lbl_colasMax);
+            Controls.Add(lbl_tiempoMaxEspera);
+            Controls.Add(gdw_iteracionesSolicitadas);
+            Name = "PantallaVisualizacion";
+            Text = "Final Simulación - Ej. 112 - Brasca Marina";
+            Load += PantallaVisualizacion_Load;
+            ((System.ComponentModel.ISupportInitialize)gdw_iteracionesSolicitadas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView gdw_iteracionesSolicitadas;
+        private Label lbl_tiempoMaxEspera;
+        private Label lbl_colasMax;
+        private TextBox txt_PorcUrgenciasAtend;
+        private TextBox txt_colaMaxConsulta;
+        private TextBox txt_colaMaxUrgencia;
+        private TextBox txt_colaMaxTotal;
+        private Label lbl_colaMaxTotal;
+        private Label lbl_colaMaxConsulta;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txt_maxEsperaUrgencia;
+        private TextBox txt_maxEsperaConsulta;
+        private Label label4;
+    }
+}
